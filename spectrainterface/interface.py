eaafd5707e21deb8a6c26eb790d32eeddc67046f
Jefferson Barros Vieira <jefferson.vieira@lnls.br>
2026-04-10 10:46:14 -0300

    fix bug kmax power calculations

--- spectrainterface/interface.py
+++ spectrainterface/interface.py
@@ -4421,7 +4421,11 @@ class SpectraInterface:
             spectra_calc.calc.CalcConfigs.Output.power_density
         )
         if source.source_type != 'bendingmagnet':
-            kmax = source.calc_max_k(spectra_calc.accelerator)
+            if source.min_gap != 0:
+                beff = source.get_beff(source.min_gap / source.period)
+                kmax = source.undulator_b_to_k(b=beff, period=source.period)
+            else:
+                kmax = source.calc_max_k(spectra_calc.accelerator)
             if source.polarization == 'hp':
                 spectra_calc.calc.source_type = (
                     spectra_calc.calc.SourceType.horizontal_undulator
@@ -4501,7 +4505,11 @@ class SpectraInterface:
 
         spectra_calc.calc.target_energy = 0
         if source.source_type != 'bendingmagnet':
-            kmax = source.calc_max_k(spectra_calc.accelerator)
+            if source.min_gap != 0:
+                beff = source.get_beff(source.min_gap / source.period)
+                kmax = source.undulator_b_to_k(b=beff, period=source.period)
+            else:
+                kmax = source.calc_max_k(spectra_calc.accelerator)
             if source.polarization == 'hp':
                 spectra_calc.calc.source_type = (
                     spectra_calc.calc.SourceType.horizontal_undulator
